de5f49e7929b6ded4a11ceb4a3af36940e11a572
Julius Reade <julius.reade@gmail.com>
2024-02-05 13:37:04 +1100

    feat: first iteration

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import githubLogo from './assets/github.svg'
+// import linkedinLogo from './assets/linkedin.svg'
 import './App.css'
 
+// import Footer from './Footer/Footer'
+// import Employment from './Employment/Employment'
+
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <div className="content">
+          <h1>Julius Reade 👋</h1>
+          <h2>I build things for the web.</h2>
+        </div>
+
+        <div className="gif__shadow"></div>
+
+        <div className="bottom">
+          <div>
+            <a href="https://www.linkedin.com/in/juliusreade/"><h3>Employment</h3></a>
+          </div>
+          <div>
+            <a href="https://github.com/dottjt"><h3>Side Projects</h3></a>
+          </div>
+        </div>
+
     </>
   )
+
 }
 
 export default App
